35c1c4f9715465620d030304a93ba776ae98aa52
Pratap Rockerss <pratap@techcetra.com>
2020-05-11 19:44:34 +0530

    added bootstrap and layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="wrapper">
+            Welcome to OnlineGuruCool
+        </div>
+    );
 }
 
 export default App;
