5a757677d6c1035e41c8f3783c6e849ecc3912a9
Pratap Rockerss <pratap@techcetra.com>
2020-05-18 07:08:06 +0530

    Todo List added for useState video

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Container } from "reactstrap";
+import TodoList from "./Components/TodoList";
 
 function App() {
     return (
         <div className="wrapper">
             <Container className="py-4">
-                <h1>Welcome to <mark className="highlight">Online GuruCool</mark></h1>
+                <h1>
+                    Welcome to{" "}
+                    <mark className="highlight">Online GuruCool</mark>
+                </h1>
+                <TodoList />
             </Container>
         </div>
     );
---
+++ src/Components/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Make Coffee</li>
+            <li>Bake Cake</li>
+            <li>Go To Gym </li>
+            <li>Read Books</li>
+        </ul>
+    );
+}
+
+export default TodoList;
