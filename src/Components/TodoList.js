c64d6a9aec4b23d5256aeb7bc3e8d067e1f32574
Pratap Rockerss <pratap@techcetra.com>
2020-05-19 08:35:46 +0530

    useState Video task done video finished

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const TodoList = () => {
+    let [todoList, setTodoList] = useState([
+        {
+            id: 1,
+            todo: "Make Coffee",
+            status: false,
+        },
+        {
+            id: 2,
+            todo: "Bake Cake",
+            status: false,
+        },
+        {
+            id: 3,
+            todo: "Go To Gym",
+            status: true,
+        },
+        {
+            id: 4,
+            todo: "Read Books",
+            status: false,
+        },
+        {
+            id: 5,
+            todo: "Create Video",
+            status: false,
+        },
+    ]);
+    const markDone = (index) => {
+        let newState = [...todoList];
+        newState[index].status = true;
+        setTodoList(newState);
+    }
     return (
         <ul>
-            <li>Make Coffee</li>
-            <li>Bake Cake</li>
-            <li>Go To Gym </li>
-            <li>Read Books</li>
+            {todoList.map((t, ti) => {
+                return (
+                    <li key={t.id} onClick={() => markDone(ti)}>
+                        {" "}
+                        {t.todo} {t.status && <i class="fa fa-check"></i>}{" "}
+                    </li>
+                );
+            })}
         </ul>
     );
-}
+};
 
 export default TodoList;
