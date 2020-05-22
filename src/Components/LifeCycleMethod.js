c6dc9a216b8b1542996ab6c01aa035570a73999c
Pratap Rockerss <pratap@techcetra.com>
2020-05-22 07:10:14 +0530

    Final Video re created for with new example

--- src/Components/LifeCycleMethod.js
+++ src/Components/LifeCycleMethod.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 const LifeCycleMethod = () => {
-    return (
-        <div>
-            This is LifeCycleMethod Component
-        </div>
-    );
-}
+    let [temp,setTemp] = useState(3)
+    useEffect(() => {
+        console.log("Render");
+        return () => {
+            console.log("UnMounting");
+        };
+    },[temp]);
+
+    useEffect(() => {
+        console.log("2nd use Effect")
+    },[])
+    return <>
+        <div>This is LifeCycleMethod Component</div>
+        <h4>Temp: {temp} </h4>
+        <button onClick={() => setTemp(23)}>
+            Set Temp
+        </button>
+    </>;
+};
 
 export default LifeCycleMethod;
