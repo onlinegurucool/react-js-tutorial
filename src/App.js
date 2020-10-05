2b1253804b3249f20aceb4a680ba78515cd509b4
Pratap Rockerss <pratap@techcetra.com>
2020-10-05 09:20:46 +0530

    App and hedaer component added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from './components/Header';
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <div>Hello World</div>
+            <div>Hello World</div>
+        </>
+    )
+};
+
+export default App;
