359d052f621d39c827cd16f197efb4d9a5e82b70
Pratap Rockerss <pratap@techcetra.com>
2020-10-06 06:30:21 +0530

    props & chaild

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import React from "react";
 import Header from './components/Header';
 
 const App = () => {
+    // function to increment i
+    var i = 0;
+    let inc = () => {
+        i++
+        console.log({i})
+    }
+
     return (
         <>
-            <Header />
-            <div>Hello World</div>
+            <Header title="I am Header" increment={inc}>
+                I am child of header
+            </Header>
             <div>Hello World</div>
         </>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-const Header = () => {
+const Header = (props) => {
+    // logging props
+    console.log(props);
+    // destructing props into variables
+    let { title, children, increment } = props;
     return (
-        <header>
-            This is a header
+        <header title={title}>
+            <br />
+            {children}
+            <button onClick={() => increment()}>Increase count</button>
         </header>
-    )
+    );
 };
 
 export default Header;
