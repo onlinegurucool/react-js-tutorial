e24457bd583f3df6d2a0e84f8b1bfd8a2edd9b75
Pratap Rockerss <pratap@techcetra.com>
2020-10-08 06:27:45 +0530

    class component example added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {Component} from "react";
 
-const Header = () => {
-    return (
-        <header>
-            This is a header
-        </header>
-    )
-};
+// const Header = () => {
+//     return (
+//         <header>
+//             This is a header
+//         </header>
+//     )
+// };
+
+class Header extends Component {
+
+    render() {
+        return (
+            <header>
+                This is a header
+            </header>
+        )
+    }
+}
 
 export default Header;
