2fb8cdf387f8962184dad2048f5612b78bcd93a3
Pratap Rockerss <pratap@techcetra.com>
2020-10-08 06:40:22 +0530

    props added in class component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Header from './components/Header';
 
-const App = () => {
-    // function to increment i
-    var i = 0;
-    let inc = () => {
-        i++
-        console.log({i})
-    }
+// const App = () => {
+//     // function to increment i
+//     var i = 0;
+//     let inc = () => {
+//         i++
+//         console.log({i})
+//     }
 
-    return (
-        <>
-            <Header title="I am Header" increment={inc}>
-                I am child of header
-            </Header>
-            <div>Hello World</div>
-        </>
-    )
-};
+    // return (
+    //     <>
+    //         <Header title="I am Header" increment={inc}>
+    //             I am child of header
+    //         </Header>
+    //         <div>Hello World</div>
+    //     </>
+    // )
+// };
+
+class App extends Component {
+    constructor(props) {
+        super();
+        console.log({AppProps: props})
+    }
+    render() {
+        return (
+            <>
+                <Header title="I am Header" 
+                    // increment={inc}
+                >
+                    I am child of header
+                </Header>
+                <div>Hello World</div>
+            </>
+        )
+    }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import App from './App';
 
 ReactDOM.render(
-    <App /> ,
+    <App temp="123" /> ,
     document.getElementById("root")
 );
 
