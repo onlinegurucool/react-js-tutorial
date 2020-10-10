ac4d893c7f57837175c41e91a06b6d43ff12e875
Pratap Rockerss <pratap@techcetra.com>
2020-10-10 05:37:06 +0530

    normal state example added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from "react";
 
 class App extends Component {
-    state = {};
+    state = {
+        count: 1,
+        cars: ["Maruti","Tata","ford"]
+    }
+
+    incCount = () => {
+        this.setState({
+            count: this.state.count + 1,
+            cars: [...this.state.cars,"BMW"]
+        })
+    }
+
+    
+
     render() {
+        let { count ,cars} = this.state;
         return (
             <>
                 <h1> Let's Lear React State</h1>
-                <p>Count: </p>
+                <p>Count: {count} </p>
+                <button onClick={this.incCount}>
+                    Click to increment
+                </button>
+                <hr />
+                <ul>
+                    {
+                        cars.map((c) => {
+                            return (
+                                <li>{c}</li>
+                            )
+                        })
+                    }
+                </ul>                
             </>
         );
     }
